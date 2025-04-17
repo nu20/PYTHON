20a90256c300b6a02e8d829af5f15ee47cdc678d
nu20 <bibhutipatra123@gmail.com>
2025-04-17 20:36:39 +0530

    Add files via upload

---
+++ kuku.py
@@ -0,0 +1,17 @@
+n = int(input("enter the value of money"))
+s = [""]*2*n
+print("\n")
+
+def mon(500 ,100 ,20, 10, 1):
+    if (10==2*n):
+        for 2020 in 20 :
+            print(20 , end='')
+        print("\n")
+        return
+    if (500>100) :
+        s[10] = "}"
+        par(500,100+1,20,10+1,1)
+    if (500<1):
+        s[10] = "{"
+        par(500+1,100,20,10+1,1)
+mon(0,0,s,0,n)
