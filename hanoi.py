4c025db0cad340d396c92f38d78d9f1fd41021ec
nu20 <bibhutipatra123@gmail.com>
2025-04-26 17:51:27 +0530

    Add files via upload

---
+++ hanoi.py
@@ -0,0 +1,9 @@
+def hanoi (n , a , b , c):
+    if n==1:
+        print("move disk 1 from rod " , a ,"to rod" , b)
+        return
+    hanoi (n-1 , a ,b ,c)
+    print("move disk" , n , "from rod" , a , "to rod"  ,b)
+    hanoi(n-1  ,c , b , a)
+n=4
+hanoi(n , "a" , "c" , "b")
---
+++ jk.py
@@ -0,0 +1,6 @@
+keypad = ["" ,"" , "abc" , "def" "ghi" , "jkl" , "mno" , "pqrs" , "tuv" , "wxyz"]
+def combination(comb , curr , output , n):
+    if (curr==n):
+        print(*output , sep=",")
+        return
+an f
