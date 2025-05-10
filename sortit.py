fdd100226a9f7b0b9689d3441ea29fe4af051cde
nu20 <bibhutipatra123@gmail.com>
2025-05-10 20:49:34 +0530

    Update sortit.py

---
+++ sortit.py
@@ -0,0 +1,23 @@
+ def sort_012(arr):
+  low = 0
+  mid = 0
+  high = len(arr) - 1
+ 
+
+  while mid <= high:
+  if arr[mid] == 0:
+  arr[low], arr[mid] = arr[mid], arr[low]
+  low += 1
+  mid += 1
+  elif arr[mid] == 1:
+  mid += 1
+  else:
+  arr[mid], arr[high] = arr[high], arr[mid]
+  high -= 1
+  return arr
+ 
+
+ # Example usage:
+ arr = [0, 1, 2, 1, 0, 2]
+ sorted_arr = sort_012(arr)
+ print(sorted_arr)  # Output: [0, 0, 1, 1, 2, 2]
